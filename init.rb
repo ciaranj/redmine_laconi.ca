6da94760ae46f3e5bc92c62eca20ea22e180c101
ciaranj <ciaranj@gmail.com>
2009-06-26 22:19:06 +0100

    Added in basic settings configuration

--- init.rb
+++ init.rb
@@ -1,13 +1,12 @@
-# Empty redmine plguin
 require 'redmine'
 
-# TODO: Change this to use the name of your plugin
 RAILS_DEFAULT_LOGGER.info 'Starting laconi.ca plugin for RedMine'
 
-# TODO: Change the name 
-Redmine::Plugin.register :empty_plugin do
+Redmine::Plugin.register :laconica_plugin do
   name 'laconi.ca plugin'
   author 'Ciaran Jessup'
   description 'This plugins integrates redmine with an existing laconi.ca installation'
   version '0.0.1'
+  
+  settings :default => {'server_url' => 'http://identi.ca/', 'server_username' => '', 'server_password'=>''}, :partial => 'settings/laconica_settings'
 end
