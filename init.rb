3241c4aaf52af73a6d1cb82a59670e820bf19ba3
ciaranj <ciaranj@gmail.com>
2009-06-26 21:44:14 +0100

    Initial commit to state purpose

--- init.rb
+++ init.rb
@@ -2,12 +2,12 @@
 require 'redmine'
 
 # TODO: Change this to use the name of your plugin
-RAILS_DEFAULT_LOGGER.info 'Starting Empty plugin for RedMine'
+RAILS_DEFAULT_LOGGER.info 'Starting laconi.ca plugin for RedMine'
 
 # TODO: Change the name 
 Redmine::Plugin.register :empty_plugin do
-  name 'Empty plugin'
-  author 'Eric Davis'
-  description 'This is an empty plugin for Redmine that is used to start new plugins'
-  version '0.0.0'
+  name 'laconi.ca plugin'
+  author 'Ciaran Jessup'
+  description 'This plugins integrates redmine with an existing laconi.ca installation'
+  version '0.0.1'
 end
